099bf6591e886640097f272e1ae0693978e5db70
Dommert <DommertEnterprises@Gmail.com>
2014-06-22 04:47:17 -0700

    updates
    flask-peewee updates

--- flask/peewee/peewee_snippet.py
+++ flask/peewee/peewee_snippet.py
@@ -1,12 +1,33 @@
 # Dommert - PeeWee Notes
+# -------------------------------
 
-# Database Connection
-database = type(DATABASE_)
+from flask import Flask
+from peewee import *
+
+# flask-peewee bindings
+from flask_peewee.db import Database
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Database 
+DATABASE = {
+    'name': 'my_database',
+    'engine': 'peewee.MySQLDatabase',
+    'user': 'db_user',
+    'passwd': 'secret password',
+    }
+DEBUG = True
+SECRET_KEY = 'Secret Key!!'
+
+db = Database(app)
+
 
 # BaseModel Connection
 class BaseModel(Model):
     class Meta:
         database = database
+
 
 # User Class Model
 class User(BaseModel)
@@ -16,3 +37,7 @@ class User(BaseModel)
 
 	Class Meta:
 		order_by = ('username',)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
