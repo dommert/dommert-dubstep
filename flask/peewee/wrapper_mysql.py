eafdeb0768974378dcb6c255575e1e4da0f47917
Dommert <DommertEnterprises@Gmail.com>
2014-07-04 06:02:29 -0700

    mysql
    mysql wrapper

---
+++ flask/peewee/wrapper_mysql.py
@@ -0,0 +1,19 @@
+# Flask-PeeWee MySQL
+
+from flask import Flask
+from peewee import *
+
+from flask_peewee.db import Database
+
+DATABASE = {
+    'name': 'my_database',
+    'engine': 'peewee.MySQLDatabase',
+    'user': 'db_user',
+    'passwd': 'secret password',
+}
+
+app = Flask(__name__)
+app.config.from_object(__name__) # load database configuration from this module
+
+# instantiate the db wrapper
+db = Database(app)
