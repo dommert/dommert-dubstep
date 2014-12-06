da5a3c363454d64baa7327359febe1fb66ddf184
Dommert <DommertEnterprises@Gmail.com>
2014-12-06 01:52:58 -0700

    ajax

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<?PHP
+// Jquery AJAZ & PHP
+// Dommert Enterprises
+//
+
+?>
+
+<html>
+<head>
+<title> Dommert's Test </title>
+</head>
+<body>
+<content>
+Test 1 - Button Ajax <a href="button_click.php">Button Click</a><BR>
+Test 2 - Button Load <a href="load_page.php">Load Page</a><br>
+Test 3 - Alert Button <a href="alert_button.php">Alert</a><BR>
+Test 4 - Search Results <a href="get_ajaxpage.php">Search</a><BR>
+
+
+</content>
+</body>
+</html>
