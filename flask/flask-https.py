39afc6497cf068c90569c22e92910f2d5ad9f13a
Dommert <DommertEnterprises@Gmail.com>
2015-05-05 02:23:24 -0700

    updates

--- flask/flask-https.py
+++
@@ -1,8 +0,0 @@
-from OpenSSL import SSL
-
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('yourserver.key')
-context.use_certificate_file('yourserver.crt')
-
-app.run(host='127.0.0.1',port='12344', 
-        debug = False/True, ssl_context=context)
