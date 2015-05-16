936a7b44f298b678081ad061e8bd5c671ffdc951
Dommert <DommertEnterprises@Gmail.com>
2015-05-15 20:34:45 -0700

    random id

---
+++ Python/random_id.py
@@ -0,0 +1,10 @@
+# Dommert
+# Random ID Generator
+
+import random, string
+myrg = random.SystemRandom()
+length = 10
+# If you want non-English characters, remove the [0:52]
+alphabet = string.letters[0:52] + string.digits
+pw = str().join(myrg.choice(alphabet) for _ in range(length))
+print pw
