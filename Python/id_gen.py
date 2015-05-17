ef2c81e9f40c6a0278883560de5020f0a68e870e
Dommert <DommertEnterprises@Gmail.com>
2015-05-17 02:49:57 -0700

    updates

--- Python/id_gen.py
+++ Python/id_gen.py
@@ -3,8 +3,8 @@
 import random, string
 
 def id_gen(length = 10):
-	myrg = random.SystemRandom()
+	rand = random.SystemRandom()
 	# If you want non-English characters, remove the [0:52]
 	alphabet = string.letters[0:52] + string.digits
-	pw = str().join(myrg.choice(alphabet) for _ in range(length))
-	return pw
+	data = str().join(rand.choice(alphabet) for _ in range(length))
+	return data
